50e9d89bcb644ad65c591f03b3d47ebdcb573901
LiorGandin <85108100+LiorGandin@users.noreply.github.com>
2024-06-17 21:22:42 +0300

    Update rti.go

--- rti/rti.go
+++ rti/rti.go
@@ -2,6 +2,7 @@ package rti
 
 // #cgo linux,amd64 CFLAGS: -I${SRCDIR}/include -I${SRCDIR}/rticonnextdds-connector/include -DRTI_UNIX -DRTI_LINUX -DRTI_64BIT
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/static_lib/x64Linux2.6gcc4.4.5 -lrtiddsconnectorz -lluaz -lnddscz -lnddscorez -ldl -lnsl -lm -lpthread -lrt
+import "C"
 
 import (
     "github.com/dop251/goja"
