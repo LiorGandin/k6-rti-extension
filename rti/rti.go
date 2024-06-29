a0b1ab49f3c33842cc98d81e5771e7ecb62b9442
LiorGandin <85108100+LiorGandin@users.noreply.github.com>
2024-06-29 12:34:52 +0300

    Update rti.go

--- rti/rti.go
+++ rti/rti.go
@@ -50,7 +50,7 @@ func (r *RTIModule) WriteRealTimeData(jsonData string) string {
         return "RTI Connector not initialized"
     }
 
-    output, err := r.connector.GetOutput("MyPublisher::MySquareWriter")
+    output, _ := r.connector.GetOutput("MyPublisher::MySquareWriter")
     if output == nil {
         return "Failed to get output"
     }
